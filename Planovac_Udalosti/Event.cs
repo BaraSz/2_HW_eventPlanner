21bfd721b3170196f9f2f585c545b2e6048472a5
Václav Kučera <vaclav.kucera@mewssystems.com>
2025-05-22 10:21:40 +0200

    HW feedback

--- Planovac_Udalosti/Event.cs
+++ Planovac_Udalosti/Event.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
-
 namespace Planovac_Udalosti
 {
     public class Event
     {
         public string NameOfEvent { get; set; }
         public DateTime DateOfEvent { get; set; }
-        
+
         public Event(string nameOfEvent, DateTime dateOfEvent)
         {
             NameOfEvent = nameOfEvent;
@@ -20,25 +14,20 @@ namespace Planovac_Udalosti
         {
             DateOfEvent = DateOfEvent.Date;
             DateTime acctualDate = DateTime.Now.Date;
-            int dayLeft;
+            int daysLeft = Math.Abs((DateOfEvent - acctualDate).Days);
 
             if (DateOfEvent > acctualDate)
             {
-                dayLeft = (DateOfEvent - acctualDate).Days;
-                Console.WriteLine($"EVENT: {NameOfEvent}, with date {DateOfEvent:yyyy-MM-dd} will happend in {dayLeft} days");
+                Console.WriteLine($"EVENT: {NameOfEvent}, with date {DateOfEvent:yyyy-MM-dd} will happend in {daysLeft} days");
             }
-
             else if (DateOfEvent < acctualDate)
             {
-                dayLeft = (acctualDate - DateOfEvent).Days;
-                Console.WriteLine($"EVENT: {NameOfEvent}, with date {DateOfEvent:yyyy-MM-dd} happend {dayLeft} days ago");
+                Console.WriteLine($"EVENT: {NameOfEvent}, with date {DateOfEvent:yyyy-MM-dd} happend {daysLeft} days ago");
             }
-
             else
             {
                 Console.WriteLine($"EVENT: {NameOfEvent}, with date {DateOfEvent:yyyy-MM-dd}  is today");
             }
-
         }
     }
 }
